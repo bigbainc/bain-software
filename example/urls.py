f807e11cd96fd9a8c2013d46cd474019a117136a
TheRustedBeasty <bchangson@proton.me>
2025-11-14 17:08:04 +1100

    copying tutorial

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,12 @@
 # example/urls.py
-from django.urls import path
-
+from django.urls import include, path
+from django.shortcuts import render, HttpResponse
 from example.views import index
 
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
 
 urlpatterns = [
     path('', index),
+    path('login/', include('login.urls')),
 ]
