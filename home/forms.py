0344f5dcc1f9b9ccd07088d90d8e096e7f8b433f
TheRustedBeasty <bchangson@proton.me>
2025-12-04 13:54:18 +1100

    forms

--- home/forms.py
+++ home/forms.py
@@ -6,9 +6,9 @@ class questionForm(forms.ModelForm):
         model = Question
         fields = ["question", "solution1", "solution2", "solution3", "solution4"]
         widgets = {
-            "Question": forms.Textarea(attrs={"rows":4, "cols":40}),
-            "Solution1": forms.Textarea(attrs={"rows":4, "cols":40}),
-            "Solution2": forms.Textarea(attrs={"rows":4, "cols":40}),
-            "Solution3": forms.Textarea(attrs={"rows":4, "cols":40}),
-            "Solution4": forms.Textarea(attrs={"rows":4, "cols":40}),
+            "question": forms.TextInput(attrs={"rows":4, "cols":40, 'placeholder':"Question...", 'class':'question-form'}),
+            "solution1": forms.TextInput(attrs={"rows":4, "cols":40, 'class':'answer-form'}),
+            "solution2": forms.TextInput(attrs={"rows":4, "cols":40, 'class':'answer-form'}),
+            "solution3": forms.TextInput(attrs={"rows":4, "cols":40, 'class':'answer-form'}),
+            "solution4": forms.TextInput(attrs={"rows":4, "cols":40, 'class':'answer-form'}),
         }
--- home/views.py
+++ home/views.py
@@ -4,24 +4,16 @@ from .forms import questionForm
 
 
 def home_view(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        form = questionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = questionForm()
+    context = {'form': form}
+
+    return render(request, 'home.html', context)
 
 def test_view(request):
     return render(request, 'test.html')
-
-def addQuestion(request):
-    if request.method == "POST":
-        form = Question(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("add_question")
-    else:
-        form = questionForm()
-    entries = Question.objects.order_by("-id")[:20]
-    return render(request, "home/home.html", {"form": form, "entries": entries})
-
-def getQuestion(request):
-    model = Question
-    templateName = "home/home.html"
-    def get_queryset(self):
-        return Question.objects()
