56d740aa1d1aa153d373dfda06f135a4c07e0c24
TheRustedBeasty <bchangson@proton.me>
2025-12-04 12:49:29 +1100

    set static directory

--- api/settings.py
+++ api/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'home' / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
